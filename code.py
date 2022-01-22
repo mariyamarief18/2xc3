0d2f19506e30cf646154c9381948686c21a0d614
mariyamarief18 <70153236+mariyamarief18@users.noreply.github.com>
2022-01-21 21:42:20 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
+def are_valid_groups(stu_no, groups):
+    ans = False
+    for i in stu_no:
+        for j in groups:
+            if i in j:
+                ans = True
+    return ans
 
+print(are_valid_groups([1,2,3,4,5,6,7,8,9], [[1,2,3],[4,5,6],[7,8,9]]))
+            
